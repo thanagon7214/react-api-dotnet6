7d7b69155c2f0f15f3876c7df3410db1bdc9101d
Countboss <108651491+thanagon7214@users.noreply.github.com>
2024-08-09 16:45:43 +0700

    update file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import React, { useEffect, useState } from 'react';
 import TableDataLayout from './page/main/tableDataLayout';
+import './vendor/css/main.css';
 
 
 
--- src/page/main/tableDataLayout.js
+++ src/page/main/tableDataLayout.js
@@ -1,28 +1,54 @@
 import React, { Component } from 'react';
+import { Stack,Table } from 'react-bootstrap';
 
 const TableDataLayout  = (props) => {
     
         return (
-            <div>
-                    <table>
-                        <thead>
-                            <tr>
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>Price</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {props.data.map(product => (
-                            <tr key={product.id}>
-                                <td>{product.id}</td>
-                                <td>{product.name}</td>
-                                <td>{product.price}</td>
-                            </tr>
-                            ))}
-                        </tbody>
-                    </table>
-               
+            <div className="div-table">
+                <Stack direction="horizontal" gap={3} className="justify-content-center">
+                    <div className="table-1">
+                        <Table striped bordered hover variant="dark">
+                            <thead>
+                                <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Price</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {props.data.map(product => (
+                                <tr key={product.id}>
+                                    <td>{product.id}</td>
+                                    <td>{product.name}</td>
+                                    <td>{product.price}</td>
+                                </tr>
+                                ))}
+                            </tbody>
+                        </Table>
+                    </div>
+                </Stack>
+                <Stack direction="horizontal" gap={3} className="justify-content-center">
+                    <div className="table-1">
+                        <Table striped bordered hover variant="dark">
+                            <thead>
+                                <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Price</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {props.data.map(product => (
+                                <tr key={product.id}>
+                                    <td>{product.id}</td>
+                                    <td>{product.name}</td>
+                                    <td>{product.price}</td>
+                                </tr>
+                                ))}
+                            </tbody>
+                        </Table>
+                    </div>
+                </Stack>
             </div>
         );
     
